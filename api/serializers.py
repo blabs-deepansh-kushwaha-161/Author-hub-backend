52eaedfe7bad4a1373a5f141f08053122420f3e2
Deepansh Kushwaha <deepansh.kushwaha@benthonlabs.com>
2025-10-22 17:40:20 +0530

    create user functionality

--- api/serializers.py
+++ api/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        fields = ('username', 'password', 'password2', 'email')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -26,8 +26,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
+           
         )
 
         user.set_password(validated_data['password'])
